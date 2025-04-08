70eaacf07bbee2ea1018e1dfa2cc8b29a92ccba2
Javadoff <sabuhi.javadli@solid.az>
2025-04-08 18:20:55 +0400

    settings fixed

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,16 @@
 from typing import Optional
 from pydantic.v1 import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:123456@localhost:5432/postgres"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:123456@localhost:5432/postgres")
     
-    SECRET_KEY: str = "sdfkjndslfksdflkdfj"
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "sdfkjndslfksdflkdfj")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
     
-    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8000"]
+    BACKEND_CORS_ORIGINS: list[str] = os.getenv("BACKEND_CORS_ORIGINS", ["http://localhost:3000", "http://localhost:8000"])
     
     API_STR: str = "/api"
     PROJECT_NAME: str = "FastAPI Project"
